ee47afd31a3e32eb13cb869ad63d195885bfa7db
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-06-28 02:16:27 -0400

    Update

--- src/toPoint.js
+++ src/toPoint.js
@@ -1,21 +1,32 @@
 import { UTMtoLL } from "./UTMtoLL";
 import { decode } from "./decode";
 
+const compose = f => g => x => f(g(x));
+
+const add = x => y => x + y;
+const divide = divisor => operand => operand / divisor;
+const divideByTwo = divide(2);
+
 /**
  * @param {String} mgrs
  * @return {*}
  */
 function toPoint(mgrs) {
   if (mgrs === "") {
-    throw new TypeError("toPoint received a blank string");
+    throw new TypeError("Arg `mgrs` cannot be a blank string");
   }
 
-  const bbox = UTMtoLL(decode(mgrs.toUpperCase()));
+  const { lon, lat, left, right, top, bottom } = UTMtoLL(
+    decode(mgrs.toUpperCase())
+  );
 
-  return bbox.lat && bbox.lon
-    ? [bbox.lon, bbox.lat]
-    : [(bbox.left + bbox.right) / 2, (bbox.top + bbox.bottom) / 2]
-  ;
+  return lon && lat
+    ? { x: lon, y: lat }
+    : {
+        x: compose (divideByTwo) (add(left)) (right),
+        y: compose (divideByTwo) (add(top)) (bottom)
+      }
+    ;
 }
 
 export { toPoint };
