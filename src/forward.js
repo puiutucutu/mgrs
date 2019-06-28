bf10ed8c5a1b8529f510be09535107369cc53b3a
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-06-28 02:03:58 -0400

    Fix jsdoc

--- src/forward.js
+++ src/forward.js
@@ -2,26 +2,24 @@ import { LLtoUTM } from "./LLtoUTM";
 import { encode } from "./encode";
 
 /**
- * Conversion of lat/lon to MGRS.
+ * Conversion of longitude and latitude to MGRS.
  *
- * @param {object} ll Object literal with lat and lon properties on a
- *     WGS84 ellipsoid.
- * @param {int} accuracy Accuracy in digits (5 for 1 m, 4 for 10 m, 3 for
- *      100 m, 2 for 1000 m or 1 for 10000 m). Optional, default is 5.
- * @return {string} the MGRS string for the given location and accuracy.
+ * @param {Object} LL Object literal with lat and lon properties on a WGS84 ellipsoid.
+ * @param {Number} [accuracy=5] Accuracy in digits (5 for 1 m, 4 for 10 m, 3 for 100 m, 2 for 1,000 m or 1 for 10,000 m).
+ * @return {string} the MGRS  string for the given location and accuracy.
  */
-function forward(ll, accuracy = 5) {
-  if (!Array.isArray(ll)) {
+function forward(LL, accuracy = 5) {
+  if (!Array.isArray(LL)) {
     throw new TypeError("forward did not receive an array");
   }
 
-  if (typeof ll[0] === "string" || typeof ll[1] === "string") {
+  if (typeof LL[0] === "string" || typeof LL[1] === "string") {
     throw new TypeError(
       "forward received an array of strings, but it only accepts an array of numbers."
     );
   }
 
-  const [lon, lat] = ll;
+  const [lon, lat] = LL;
   if (lon < -180 || lon > 180) {
     throw new TypeError(`forward received an invalid longitude of ${lon}`);
   }
