1a455971f35fd1ac92729ead1ae5c71d2c6b3f4f
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-06-28 01:37:40 -0400

    Code style

--- src/toPoint.js
+++ src/toPoint.js
@@ -1,15 +1,17 @@
-import { UTMtoLL } from './UTMtoLL';
+import { UTMtoLL } from "./UTMtoLL";
+import { decode } from "./decode";
 
 function toPoint(mgrs) {
-  if (mgrs === '') {
-    throw new TypeError('toPoint received a blank string');
+  if (mgrs === "") {
+    throw new TypeError("toPoint received a blank string");
   }
+
   const bbox = UTMtoLL(decode(mgrs.toUpperCase()));
-  if (bbox.lat && bbox.lon) {
-    return [bbox.lon, bbox.lat];
-  }
-  return [(bbox.left + bbox.right) / 2, (bbox.top + bbox.bottom) / 2];
+
+  return bbox.lat && bbox.lon
+    ? [bbox.lon, bbox.lat]
+    : [(bbox.left + bbox.right) / 2, (bbox.top + bbox.bottom) / 2]
+  ;
 }
 
-
-export { toPoint}
+export { toPoint };
