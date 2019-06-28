cb6cc1ccf03cfecec46263404bc9ac4143a9df99
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-06-28 01:15:06 -0400

    Export constants

--- src/constants.js
+++ src/constants.js
@@ -1,29 +1,37 @@
 /**
- * UTM zones are grouped, and assigned to one of a group of 6
- * sets.
+ * UTM zones are grouped, and assigned to one of a group of 6 sets.
  *
- * {int} @private
+ * @type {Number}
  */
 const NUM_100K_SETS = 6;
 
 /**
- * The column letters (for easting) of the lower left value, per
- * set.
+ * The column letters (for easting) of the lower left value, per set.
  *
- * {string} @private
+ * @type {String}
  */
 const SET_ORIGIN_COLUMN_LETTERS = "AJSAJS";
 
 /**
- * The row letters (for northing) of the lower left value, per
- * set.
+ * The row letters (for northing) of the lower left value, per set.
  *
- * {string} @private
+ * @type {String}
  */
 const SET_ORIGIN_ROW_LETTERS = "AFAFAF";
 
-const A = 65; // A
-const I = 73; // I
-const O = 79; // O
-const V = 86; // V
-const Z = 90; // Z
+const A = 65;
+const I = 73;
+const O = 79;
+const V = 86;
+const Z = 90;
+
+export {
+  NUM_100K_SETS,
+  SET_ORIGIN_COLUMN_LETTERS,
+  SET_ORIGIN_ROW_LETTERS,
+  A,
+  I,
+  O,
+  V,
+  Z
+};
